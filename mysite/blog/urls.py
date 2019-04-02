dcfb2e774b313986ce891b1859999aa0f4b3db72
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-02 23:38:11 +0200

    Added urls to blog and users application

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,8 +1,28 @@
 from django.urls import path, include
 from blog import views
 
+
 urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    # article CRUD Views
+    path('about/', views.AboutMeView.as_view(), name="about"),
+    path("article/new/", views.ArticleCreateView.as_view(), name="article_create"),
+    path("<slug:slug>/", views.ArticleDetailView.as_view(), name="article"),
+    path("article/edit/<int:pk>/",
+         views.ArticleUpdateView.as_view(), name="article_edit"),
+    path("article/publish/<int:pk>/",
+         views.article_publish, name="article_publish"),
+    path("article/unpublish/<int:pk>/",
+         views.article_unpublish, name="article_unpublish"),
+    path("article/delete/<int:pk>/",
+         views.ArticleDeleteView.as_view(), name="article_delete"),
+    # Draft list
+    path("article/drafts/", views.DraftListView.as_view(), name="drafts"),
+    # random article
+    path("article/random/", views.random_article, name="random_article"),
+    # about page
+
+
 
 
 ]
-
